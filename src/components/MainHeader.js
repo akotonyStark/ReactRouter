432bdd4245d183c4228d0b9cd16581cc63076513
Augustine Akoto Larbi-Ampofo <augustine.larbi-ampofo@persol.net>
2021-09-23 16:00:58 +0000

    css added

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import classes from './MainHeader.module.css'
 
 function MainHeader() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href='/welcome'>Welcome</a>
-        </li>
-        <li>
-          <a href='/products'>Products</a>
-        </li>
-      </ul>
-    </nav>
+    <header className={classes.header}>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/welcome'>Welcome</Link>
+          </li>
+          <li>
+            <Link to='/products'>Products</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
