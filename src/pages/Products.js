92df00f29f3153fb6379b15f87673b700b80840d
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-09-24 10:04:55 +0000

    Update Products.js

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,18 @@
+import { Link } from 'react-router-dom'
 const Products = () => {
   return (
     <>
       <h2>Products Page</h2>
       <ul>
-        <li>MacBook Pro</li>
-        <li>MacBook Air</li>
-        <li>iPad Pro</li>
+        <li>
+          <Link to='/products/pro'>MacBook Pro</Link>
+        </li>
+        <li>
+          <Link to='/products/air'>MacBook Air</Link>
+        </li>
+        <li>
+          <Link to='/products/ipad'>iPad Pro</Link>
+        </li>
       </ul>
     </>
   )
